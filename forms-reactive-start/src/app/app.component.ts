7fb26ae0eaf5476e5389801c0d9f099f9b4e12f1
David Hildebrandt <davihildebran@gmail.com>
2022-05-22 20:02:51 -0300

    Add reactive form

--- forms-reactive-start/src/app/app.component.ts
+++ forms-reactive-start/src/app/app.component.ts
@@ -1,10 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormArray, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
   genders = ['male', 'female'];
+  signupForm: FormGroup;
+
+  ngOnInit(): void {
+    this.signupForm = new FormGroup({
+      'userData': new FormGroup({
+        'username': new FormControl(null, [Validators.required]),
+        'email': new FormControl(null, [Validators.required, Validators.email])
+      }),
+      'gender': new FormControl('male'),
+      'hobbies': new FormArray([])
+    })
+  }
+
+  onSubmit() {
+    console.log(this.signupForm);
+    if (!this.signupForm.valid) {
+      this.signupForm.markAllAsTouched();
+    }
+  }
+
+  get getHobbies(): FormArray {
+    return this.signupForm.get('hobbies') as FormArray;
+  }
+
+  onAddHobby() {
+    this.getHobbies.push(new FormControl(null, Validators.required));
+  }
 }
