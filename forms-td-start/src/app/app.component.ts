f6a88b90e493901155d99a0e6c0bd041764f3c0d
David Hildebrandt <davihildebran@gmail.com>
2022-05-22 17:00:29 -0300

    Finalize form with ngModel

--- forms-td-start/src/app/app.component.ts
+++ forms-td-start/src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { FormGroup, NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,7 +7,55 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
+
+  @ViewChild('f')
+  form: NgForm;
+
+  user: AppData = {} as AppData;
+  defaultQuestion: string = 'pet'
+  answer = '';
+  submited:boolean = false;
+  genders: string[] = ['male', 'female'];
+
   suggestUserName() {
     const suggestedName = 'Superuser';
+    this.form.form.patchValue({
+      userData: {
+        username: suggestedName
+      }
+    })
   }
+
+  get userData(): FormGroup {
+    return this.form.controls['userData'] as FormGroup;
+  }
+
+  // onSubmit(form: NgForm):void {
+  //   console.log(form);
+  // }
+
+  onSubmit():void {
+    
+    this.submited = this.form.form.valid;
+    if (this.submited) {
+      this.user = this.form.value as AppData;
+    }
+    else {
+      this.userData.controls['username'].markAsTouched();
+      this.userData.controls['email'].markAsTouched();
+      this.form.form.controls['secret'].markAsTouched();
+    }
+  }
+}
+
+interface AppData {
+  userData: {
+    username: string,
+    email: string
+  },
+  secret: string,
+  questionAnswer: string,
+  gender: string,
+
 }
