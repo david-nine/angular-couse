566b46311b6fe9d6f82040b785ec226b7161dd4b
David Hildebrandt <davihildebran@gmail.com>
2022-05-22 18:00:19 -0300

    Finalized first forms pratice

--- forms-td-assignment-start/src/app/app.component.ts
+++ forms-td-assignment-start/src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
+  @ViewChild('f') form: NgForm;
+
+  defaultSubscription = 'advanced';
+
+  onSubmit() {
+    const form = this.form.form;
+    if (!this.form.valid) {
+      Object.keys(form.controls).forEach(function (index) {
+        form.controls[index].markAsTouched();
+      });
+      return; 
+    }
+    console.log(form);
+  }
 
 }
