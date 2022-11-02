2179c811d6effa739ac88ff0def84d491e2c1d6b
David Hildebrandt <54282964+david-nine@users.noreply.github.com>
2022-11-02 14:39:40 -0300

    Update auth-interceptor.service.ts

--- http-01-start/src/app/auth-interceptor.service.ts
+++ http-01-start/src/app/auth-interceptor.service.ts
@@ -1,6 +1,6 @@
 import { HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
 
-const FIREBASE_URL = 'https://ng-complete-guide-21e42-default-rtdb.firebaseio.com/posts.json';
+const FIREBASE_URL = 'add firebase url';
 
 export class AuthInterceptorService implements HttpInterceptor {
     intercept(req: HttpRequest<any>, next: HttpHandler) {
